188a8ae0828dbc31a56e3fb47f6ff88f8ad886d3
advikap1026 <127911640+advikap1026@users.noreply.github.com>
2023-07-28 22:35:25 -0400

    Add files via upload

--- convolve.py
+++ convolve.py
@@ -109,15 +109,16 @@ class DataAnalysis:
             return lo
 
 
-
 def main():
     data_input = DataInput()
-    data_analysis = Solution(data_input)
+    data_analysis = DataAnalysis(data_input)
     print(f"mean1A = {round(data_analysis.mean1A,2)}")
     print(f"mean2A = {round(data_analysis.mean2A,2)}")
     print(f"mean1A + mean2A = {round(data_analysis.mean1A + data_analysis.mean2A,2)}")
     print("The probability that two events will take less than t' < t:")
     print("PrA(t'< {:2.1f} s) = {:2.2f}".format(data_analysis.timeModelA, data_input.probModel))
+    output_plot = OutputPlotting(data_analysis)
+    output_plot.plot()
 
 if __name__ == "__main__":
     main()
